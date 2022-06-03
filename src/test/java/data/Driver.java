99e28bb7e5ec4cbc94836dd92e958f3abdc0633a
MarinaKrasyuk <MarinaKrasyuk@coherentsolutions.com>
2022-06-03 14:34:07 +0300

    Task 120 clouds: 03.06.2022

--- src/test/java/data/Driver.java
+++ src/test/java/data/Driver.java
@@ -4,14 +4,19 @@ import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver {
     private static WebDriver driver;
@@ -21,11 +26,15 @@ public class Driver {
 
     public static WebDriver getDriver() throws MalformedURLException {
         if (driver == null) {
-            FirefoxOptions opt=new FirefoxOptions();
-            opt.setCapability("browserVersion", "99.0");
-            opt.setCapability("enableVNC",true);
 
-            driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),opt);
+            FirefoxOptions browserOptions = new FirefoxOptions();
+            browserOptions.setPlatformName("Windows 8.1");
+            browserOptions.setBrowserVersion("100");
+            Map<String, Object> sauceOptions = new HashMap<>();
+            browserOptions.setCapability("sauce:options", sauceOptions);
+            URL url = new URL("https://oauth-marinakrasyuk-885c4:5edb7ae3-2576-467a-976e-349ccf75b270@ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+            driver = new RemoteWebDriver(url, browserOptions);
+
 
         }
         return driver;
